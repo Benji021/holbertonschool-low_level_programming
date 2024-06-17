3ddd05d3386439656aa6f16a124c4685683e901a
root <9548holbertonstudents.com>
2024-06-17 15:00:27 +0200

    langage-c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - "Programming is like building a multilingual puzzle" 
+ * main - "Programming is like building a multilingual puzzle"
  * Return: always 0
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
