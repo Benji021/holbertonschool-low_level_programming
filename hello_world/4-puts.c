69a1bde6f229396e5d5165df4ac588e790f196f1
root <root@Drakan>
2024-06-17 11:17:33 +0200

    langage-c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+ * retourn: always 0
+ */
+{	
+	puts("\"Programming is like building a multilingual puzzle\n");
+	retourn (0);
+}
