9ee7c8d1a79b376ab7cecaef2817ce54dd5a52c1
root <9548holbertonstudents.com>
2024-06-17 15:46:21 +0200

    langage-c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - "with proper grammar, but the outcome is a piece of art"
+ * main - "with proper grammar, but the outcome is a piece of art,"
  * Return Always 0
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
