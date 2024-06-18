e490325dc35eb01b8875910ed24571db6a0ef211
root <9548holbertonstudents.com>
2024-06-18 14:24:46 +0200

    task 0

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@ int main(void)
 {
 char n;
 n = 'a';
-        while (n <= 'z')
-        {
-                putchar(n);
-                n++;
-        }
-        putchar('\n');
-        return (0);
+while (n <= 'z')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
 }
