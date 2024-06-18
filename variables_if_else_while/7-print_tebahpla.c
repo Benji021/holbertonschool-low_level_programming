d572e854fa6aead0094774c26bbf07aaf84aad98
root <9548holbertonstudents.com>
2024-06-18 16:50:39 +0200

    langage-c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - 7-print_tebapla
+ * Return: Always 0
+*/
+int main(void)
+{
+	char a;
+
+	for ( a = 'z'; a>= 'a'; a--)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
