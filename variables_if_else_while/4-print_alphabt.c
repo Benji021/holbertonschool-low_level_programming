e2ce2bd29714f2c55de2a23db54ebdba550aaaf8
root <9548holbertonstudents.com>
2024-06-18 15:41:31 +0200

    langage-c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - 4-print_alphabt.c
+ * Return: Always 0
+*/
+int main(void)
+{
+char n;
+n = 'a';
+	while (n <= 'z')
+{
+	if ((letter != 'q') && (letter != 'e')
+	putchar(n);
+	n++;
+}
+{
+	putchar('\n')
+	return(0)
+}
