441fb99f25ca2b72338c84092afd57a14e9ad19b
root <9548holbertonstudents.com>
2024-06-18 15:15:22 +0200

    langage-c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,12 @@
 int main(void)
 {
 char n;
-for (n = "a"; n <= "z"; n++)
-{
-putchar(n);
-}
-for (n = "A"; n <= "Z"; n++)
-{
-putchar(n);
-}
-putchar("\n")
-return (0)
+n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
