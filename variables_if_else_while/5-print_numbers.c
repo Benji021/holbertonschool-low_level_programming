cff7f7b9789462d494a3813eeec439329c77e1ee
root <9548holbertonstudents.com>
2024-06-18 16:16:39 +0200

    langage-c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - runs a for loop to print single digits
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+	for (i = 0 ; i <= 9 ; i++)
+        {
+        printf("%d", i);
+        }
+	putchar ('\n');
+	return (0);
+}
