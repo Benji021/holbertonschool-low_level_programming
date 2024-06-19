a700e74597e9a9e9cda6a9eeebc9c5e874a56964
root <9548holbertonstudents.com>
2024-06-19 10:46:01 +0200

    langage-c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,14 +5,12 @@
 */
 int main(void)
 {
-        char n;
+	char n;
 
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar(n);
-                putchar(',');
-                putchar(' ');
-        }
-        putchar('\n');
-        return (0);
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
 }
