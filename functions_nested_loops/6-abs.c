c44d2722fa87a783e0053329836a3b058453c5de
root <9548holbertonstudents.com>
2024-06-20 18:27:41 +0200

    langage-c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - absolute
+ * @n: The number to be comp
+ * Return: the absolute value of number or 0
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int abs_val;
+
+		abs_val = n * -1;
+
+		return (abs_val);
+	}
+
+	return (n);
+}
