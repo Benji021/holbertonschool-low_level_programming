5a13275e55e1873371510fcc7e51a6058ab4b16d
root <9548holbertonstudents.com>
2024-06-20 13:02:42 +0200

    langage-c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,4 @@
 #include "main.h"
-/**
- * print_alphabet_ x10 - multiple
- * Return: Always 0
-*/
 void print_alphabet_x10(void)
 {
 	char i;
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,4 +5,6 @@ int main(void);
 void print_alphabet(void);
 int main(void);
 void print_alphabet_x10(void);
+int main(void);
+int _islower(int c);
 #endif
