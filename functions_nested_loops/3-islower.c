17ed33fc569dfef556a2493ef578f4e60429e1ec
root <9548holbertonstudents.com>
2024-06-20 13:07:07 +0200

    langage-c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * main - islower
- * Return: Always 0
+ * Return: 1 for lowercase character or 0 for anything else
 */
 int _islower(int c);
 {
-		if (c >= 'a' && c >= 'z')
+		if (c >= 'a' && c <= 'z')
 		{
 		return (1);
 		}
