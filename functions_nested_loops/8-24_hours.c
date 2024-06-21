561b577985622dea2180c54269f8b7815b99d2f2
root <9548holbertonstudents.com>
2024-06-21 09:54:39 +0200

    langage-c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,12 +8,12 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h <= 23, h++)
+	for (h = 0; h <= 23; h++)
 	{
 		_putchar((h / 10) + '0');
 		-putchar(':');
 	}
-	for (m = 0; m <= 59, m++)
+	for (m = 0; m <= 59; m++)
 	{
 		_putchar((m / 10) + '0');
 	}
