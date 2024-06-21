2daa00b2692a31400de5d9a518a0912cd2357305
root <9548holbertonstudents.com>
2024-06-21 16:03:50 +0200

    langage-c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -5,7 +5,7 @@
   * @n: The number to be treated
   *
   * Return: Value of the last digit of number
-*/
+  */
 int print_last_digit(int n)
 {
 	int last;
@@ -15,7 +15,9 @@ int print_last_digit(int n)
 	if (last < 0)
 	{
 		last = last * -1;
-		_putchar(last + '0');
 	}
-	return (n);
+
+	_putchar(last + '0');
+
+	return (last);
 }
