bb435d737cb5895ee7e7a8c5d006fa83e72b57e4
root <9548holbertonstudents.com>
2024-06-24 11:19:55 +0200

    lanage-c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,11 @@
 /**
  * _isdigit - check for a digit (0 through 9)
  * @c : character to check
- * Return: 1 is a digit or 0 else
+ * Return: 1 is a digit (0 through 9) or 0 else
 */
 int _isdigit(int c)
 {
-	for (c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
