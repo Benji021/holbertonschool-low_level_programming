544533757c100e50a55299f0b2f383581c12e0e8
root <9548holbertonstudents.com>
2024-06-25 16:40:52 +0200

    langage-c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - the values of two integers.
+ * a@ : pointer to first value
+ * b@ : pointer to second value
+ * Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	c = *b;
+	*a = *b;
+}
