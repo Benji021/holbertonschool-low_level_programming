de882a49a25828ff2987eaf280eb7f1cfda00d91
root <9548holbertonstudents.com>
2024-06-25 14:36:21 +0200

    langage-c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,16 @@ int main(void)
 	for (n = 0; n <= 100; n++)
 	{
 		if (n %3)
-		{
 			printf("Fizz");
-			printf(" ");
-		}
 		else if (n %5)
-		{
 			printf("Buzz");
-			printf(" ");
-		}
 		else if (n %3 && n %5)
-		{
 			printf("FizzBuzz");
-			printf(" ");
-		}
 		else
-		{
-			printf(n);
+			printf("%d", n);
+		if (n < 100)
 			printf(" ");
-		}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
