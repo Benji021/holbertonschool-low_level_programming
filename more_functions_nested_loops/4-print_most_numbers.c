b1a3183f84933cd7712dd2087957f4b45e8ef751
root <9548holbertonstudents.com>
2024-06-25 09:18:48 +0200

    langage-c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, do not print 2 and 4
- * Return: void
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
 */
+
 void print_most_numbers(void)
 {
-	int n;
+	char c;
 
-	int (n = '0', n <= '9', n++)
-		if (n == '2' || n == '4')
-		{
-			n++;
-		}
-		{
-		_putchar(n);
-		}
-		_putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
 }
-
