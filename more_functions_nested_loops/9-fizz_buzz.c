96e9c8994fc9190b5eb2186f50f9accd20826eb8
root <9548holbertonstudents.com>
2024-06-25 14:10:24 +0200

    langage-c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * fizz_buzz - the numbers from 1 to 100
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 100; n++)
+	{
+		if (n %3)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (n %5)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (n %3 && n %5)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else
+		{
+			printf(n);
+			printf(" ");
+		}
+	}
+	printf('\n');
+	return (0);
+}
