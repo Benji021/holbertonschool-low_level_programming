8d4e80003a0c3bebea6c151c3f397e63630f279e
root <9548holbertonstudents.com>
2024-06-25 09:14:07 +0200

    langage-c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,7 @@
 */
 void print_most_numbers(void)
 {
-	char n;
+	int n;
 
 	int (n = '0', n <= '9', n++)
 		if (n == '2' || n == '4')
@@ -16,6 +16,6 @@ void print_most_numbers(void)
 		{
 		_putchar(n);
 		}
-		_putchar('\n'),
+		_putchar('\n');
 }
 
