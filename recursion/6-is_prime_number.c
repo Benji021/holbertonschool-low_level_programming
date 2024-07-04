5cbd9eb9c47b8c296b3abd1cee77c57d12aa9ecb
root <9548holbertonstudents.com>
2024-07-04 14:01:59 +0200

    langage-c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,10 +8,18 @@
 */
 int is_prime_number(int n)
 {
-	int start = n / 2;
-
 	if (n <= 1)
 		return (0);
 
-	return (start);
+	if (n == 2)
+		return (1);
+
+	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
+		return (0);
+
+	else if (n % 9 == 0 || n % 7 == 0)
+		return (0);
+
+	else
+		return (1);
 }
