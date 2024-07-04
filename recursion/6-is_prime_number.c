30d4ceefd27601732a72156f5c33731e268c2f0a
root <9548holbertonstudents.com>
2024-07-04 13:57:58 +0200

    langage-c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,15 +4,14 @@
 /**
  * is_prime_number -  is a prime number
  * @n : int number
- * Return:  1 if the input integer is a prime number, otherwise return 0
+ * Return:  1 if prime number, 0 otherwise
 */
 int is_prime_number(int n)
 {
-	if (n < 2)
+	int start = n / 2;
+
+	if (n <= 1)
 		return (0);
 
-	if (n < 4)
-		return (1);
-
-	return (prime_number(n, 2));
+	return (start);
 }
