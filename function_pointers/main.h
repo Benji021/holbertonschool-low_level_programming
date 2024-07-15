e0dccd153055ba804c2a5a557d77d49f14fc65e3
root <9548holbertonstudents.com>
2024-07-15 09:42:18 +0200

    langage-c

---
+++ function_pointers/main.h
@@ -0,0 +1,8 @@
+#ifndef
+#define
+int _putchar(char c);
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+int main(void);
+void print_name(char *name, void (*f)(char *));
+#endif
