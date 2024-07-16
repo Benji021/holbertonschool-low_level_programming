43f0b157ae2b50387f4ab3e2ba07a73774cec82f
root <9548holbertonstudents.com>
2024-07-16 14:41:31 +0200

    langage-c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,13 +1,11 @@
-#include <unistd.h>
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
