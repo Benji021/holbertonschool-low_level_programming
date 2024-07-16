ef354e907c279785a989790dfcf8f4624671259c
root <9548holbertonstudents.com>
2024-07-16 15:06:27 +0200

    langage-c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,9 @@
+#include "variaic_function.h"
+/**
+ * sum_them_all - that returns the sum of all its parameters.
+ *@n : number of arguments passed to the function
+ * Return: 0 if n == 0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+
