9b81c53dee31e5e34e271b6f4f8ffc703d660a25
root <9548holbertonstudents.com>
2024-07-29 15:44:35 +0200

    print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_list - Write a function that prints all the elements of a list_t list
+ *
+ * Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+
---
+++ singly_linked_lists/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN
+#define MAIN
+
+int _putchar(char c);
+int main(void);
+size_t print_list(const list_t *h);
+#endif
