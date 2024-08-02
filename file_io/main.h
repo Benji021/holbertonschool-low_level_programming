f8658a6c87b6043944a354bc25299928aa3032f5
root <9548holbertonstudents.com>
2024-08-02 14:31:00 +0200

    read_textfile

---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+
+int main(int ac, char **av);
+ssize_t read_textfile(const char *filename, size_t letters);
+int main(int ac, char **av);
+int create_file(const char *filename, char *text_content);
+int main(int ac, char **av);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
